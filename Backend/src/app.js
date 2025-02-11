b099a202a75e83d2722ee2c9b875e6c5a0ce66ef
prithvikings <prithvi07raj07@gmail.com>
2025-02-12 02:53:37 +0530

    Completed the Project

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,7 +2,9 @@ const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
 const aiRoutes=require('./routes/ai.routes');
+const cors=require('cors');
 
+app.use(cors());
 dotenv.config();
 
 app.use(express.json());
